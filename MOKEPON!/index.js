c43bab58d98749e2d0cdba35a5bef99f6ac7d037
Andres2310ra <carlosandres.ramosg@outlook.com>
2023-10-11 21:44:17 -0500

    Update index.js

--- MOKEPON!/index.js
+++ MOKEPON!/index.js
@@ -1,1 +1,10 @@
-console.log('Hola NodeJS')
+const libreriaExpress = require('express')
+const app = libreriaExpress()
+
+app.get("/", (reqs, resp) => {
+    resp.send('Hola el Servidor esta en Funcionamiento')
+})
+
+app.listen(8080, () => {
+    console.log('Servidor Funcionando')
+})
