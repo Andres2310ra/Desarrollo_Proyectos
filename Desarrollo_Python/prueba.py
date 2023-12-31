1909cb56f485f68a20fcb4f1a07c0074f3dfd32a
Andres2310ra <carlosandres.ramosg@outlook.com>
2023-12-31 14:26:35 -0500

    Update prueba.py

--- Desarrollo_Python/prueba.py
+++ Desarrollo_Python/prueba.py
@@ -5,3 +5,8 @@ rutaDoc = r'C:\Users\carlo\OneDrive\Documents\GitHub\Platzi_Estudio\Desarrollo_P
 datosExcel = pd.read_excel(rutaDoc,sheet_name='VENTAS')
 
 print(datosExcel)
+
+# Agrupar por la columna 'Fruta' y calcular el promedio de la columna 'Total'
+promedio_por_fruta = datosExcel.groupby('Fruta')['Total'].mean()
+
+print(promedio_por_fruta)
