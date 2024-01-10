1ba46a5366079418d7fd79b20d69ee65e27a8aa0
Andres2310ra <carlosandres.ramosg@outlook.com>
2024-01-10 17:16:17 -0500

    Update prueba.py

--- Desarrollo_Python/prueba.py
+++ Desarrollo_Python/prueba.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import faker as fk
+import numpy as np
 
 fk = fk.Faker()
 
 numeroRegistros = 1001
 
 nombres = [fk.name() for _ in range(numeroRegistros)]
+
+hEntrada=[0.291666666666667,0.3125,0.333333333333333]
+hAleatoria=np.random.randint(1,3)
+horaEntrada=hEntrada[hAleatoria]
 
 data = {'Nombre': nombres}
 
