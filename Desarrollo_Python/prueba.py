6ca1a5e97c4f0a47d41b3954e5faee2df7e0384f
Andres2310ra <carlosandres.ramosg@outlook.com>
2024-01-10 20:08:23 -0500

    Update prueba.py

--- Desarrollo_Python/prueba.py
+++ Desarrollo_Python/prueba.py
@@ -8,10 +8,6 @@ numeroRegistros = 1001
 
 nombres = [fk.name() for _ in range(numeroRegistros)]
 
-hEntrada=[0.291666666666667,0.3125,0.333333333333333]
-hAleatoria=np.random.randint(1,3)
-horaEntrada=hEntrada[hAleatoria]
-
 data = {'Nombre': nombres}
 
 df = pd.DataFrame(data)
