01f1f47a406372abec283beaadffac5b426740ab
Andres2310ra <carlosandres.ramosg@outlook.com>
2024-01-12 15:46:37 -0500

    .

--- Desafios_Pensamiento_Logico/caso4.js
+++ Desafios_Pensamiento_Logico/caso4.js
@@ -1,24 +1,23 @@
 function solution(num) {
+  let resultado = [];
 
-    let resultado = []
-
-    function numPrimo(n) {
-        for (let i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i === 0) {
-                return false
-            }
-        }
-        return true
+  function numPrimo(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    for (let i = 2; i <= num; i++) {
-        if (numPrimo(i)) {
-            resultado.push(i)
-        }
+  for (let i = 2; i <= num; i++) {
+    if (numPrimo(i)) {
+      resultado.push(i);
     }
-    console.log(resultado)
+  }
+  console.log(resultado);
 }
 
-let valor = 100
+let valor = 100;
 
-solution(valor)
+solution(valor);
