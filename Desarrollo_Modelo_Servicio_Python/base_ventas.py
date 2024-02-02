54f969591bbcfeaab8369b05a0b4402441455e11
Andres2310ra <carlosandres.ramosg@outlook.com>
2024-02-02 17:41:46 -0500

    Update base_ventas.py

--- Desarrollo_Modelo_Servicio_Python/base_ventas.py
+++ Desarrollo_Modelo_Servicio_Python/base_ventas.py
@@ -5,6 +5,10 @@ import faker as Facker
 try:
     fk=Facker()
     np.random.seed(1)
+    numero_registros=100
+
+    nombre_pdv=[fk.country() for _ in (range(numero_registros))]
+    
 
 except Exception as e:
     print(f"Hubo un error con la base de datos: {e}")
