894de4dd45e5e9acb90eb09efe9a719f841d4210
Andres2310ra <carlosandres.ramosg@outlook.com>
2024-02-03 22:22:19 -0500

    Update base_ventas.py

--- Desarrollo_Modelo_Servicio_Python/base_ventas.py
+++ Desarrollo_Modelo_Servicio_Python/base_ventas.py
@@ -1,14 +1,30 @@
 import pandas as pd
 import numpy as np
-import faker as Facker
+import faker as Faker
 
 try:
-    fk=Facker()
-    np.random.seed(1)
-    numero_registros=100
+    fk = Faker.Faker() 
+    np.random.seed(50)
+    numero_registros = 100
+    nombre_pais_set=set()
 
-    nombre_pdv=[fk.country() for _ in (range(numero_registros))]
-    
+    while len(nombre_pais_set) < numero_registros:
+        nombre_pais_set.add(fk.country())
+
+    nombre_pais = list(nombre_pais_set)
+    venta_pais = np.random.randint(100,1000,numero_registros)
+
+    data = {
+        'PAIS VENTA': nombre_pais,
+        'VENTAS': venta_pais
+    }
+
+    df = pd.DataFrame(data)
+
+    df.to_excel(r'C:\Users\carlo\OneDrive\Documents\Desarrollo\Modelo_Servicio\Base_Ventas.xlsx', sheet_name='VENTAS')
 
 except Exception as e:
-    print(f"Hubo un error con la base de datos: {e}")
+    print(f"Hubo un error con la base de datos: {type(e).__name__}:{e}")
+
+else:
+    print('Base de Datos Terminada')
