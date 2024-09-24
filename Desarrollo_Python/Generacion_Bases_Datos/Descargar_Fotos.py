f7af0703a6ee5147c9707adc4537dbef63e6df18
Andres2310ra <77172059+Andres2310ra@users.noreply.github.com>
2024-09-24 13:58:02 -0500

    .

--- Desarrollo_Python/Generacion_Bases_Datos/Descargar_Fotos.py
+++ Desarrollo_Python/Generacion_Bases_Datos/Descargar_Fotos.py
@@ -1,11 +1,15 @@
 import os
 import requests
+import pandas as pd
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
-# Lista de URLs
-urls = [
-"https://visionstandardapp.blob.core.windows.net/data/fotosWeb/138/23029737-904d44c8-58cd-4a2b-b4d2-00a82a5ee0c0.jpg",
-"https://azpgblobladgtm1p.blob.core.windows.net/photo-storage-prod/a0G8a00002ddtL7EAIa088a00001axVjNAAUa0F8a00001JoNxQEAVa078a000015A1wTa0C8a000010jYSAEA2171785750121.jpg"
-]
+# Ruta del archivo Excel
+excel_path = r"C:\Users\carlos.ramos\OneDrive\OneDrive - Vision & Marketing S.A.S\Documents\Desarrollo\Python\Bases_Trabajo_Python\Fotos_URLs.xlsx"
+
+# Leer URLs del archivo Excel
+df = pd.read_excel(excel_path)
+urls = df['FOTOS URL'].tolist()
 
 # Ruta de la carpeta donde se guardarán las fotos
 save_folder = r"C:\Users\carlos.ramos\OneDrive\OneDrive - Vision & Marketing S.A.S\Desktop\FOTOS_DESCARGA"
@@ -15,20 +19,20 @@ if not os.path.exists(save_folder):
     os.makedirs(save_folder)
 
 # Función para descargar y guardar cada imagen
-def download_image(url, save_path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Imagen descargada: {save_path}")
-    else:
-        print(f"Error al descargar la imagen: {url}")
+def download_image(url):
+    try:
+        file_name = url.split("/")[-1]
+        save_path = os.path.join(save_folder, file_name)
 
-# Descargar cada imagen de la lista
-for url in urls:
-    # Obtener el nombre del archivo de la URL
-    file_name = url.split("/")[-1]
-    # Ruta completa para guardar la imagen
-    save_path = os.path.join(save_folder, file_name)
-    # Descargar la imagen
-    download_image(url, save_path)
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(save_path, 'wb') as file:
+                file.write(response.content)
+        else:
+            print(f"Error al descargar la imagen: {url}")
+    except Exception as e:
+        print(f"Error durante la descarga: {url}. Detalles: {e}")
+
+# Usar ThreadPoolExecutor para descargar en paralelo
+with ThreadPoolExecutor(max_workers=10) as executor:  # Puedes ajustar el número de hilos (max_workers)
+    list(tqdm(executor.map(download_image, urls), total=len(urls), desc="Descarga de Fotos"))
---
+++ Desarrollo_Python/Generacion_Bases_Datos/import random.py
@@ -0,0 +1,4 @@
+import random
+
+numeros_aleatorios = random.sample(range(1, 9), 8)
+print(numeros_aleatorios)
