99c77124dd74cdfdb7d6808a30eb0ebe247e2ab4
Andres2310ra <77172059+Andres2310ra@users.noreply.github.com>
2025-03-04 16:11:17 -0500

    .

--- Udemy/Curso_Programacion/sites/js/index.js
+++ Udemy/Curso_Programacion/sites/js/index.js
@@ -2,8 +2,12 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    let valor_1 = Math.floor(Math.random() * 100) + 1;
+    const valor_1 = Math.floor(Math.random() * 100) + 1;
     let Valor_2 = Math.floor(Math.random() * 100) + 1;
+
+    // valor_1=1
+    Valor_2=1
+
     let Resultado = valor_1 + Valor_2;
 
     document.getElementById('resultado').textContent = 'El resultado de la suma es: ' + Resultado;
