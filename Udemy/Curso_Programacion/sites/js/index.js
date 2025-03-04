d5637f3179c40c62bd19fb0f74a36ccd7c28179e
Andres2310ra <77172059+Andres2310ra@users.noreply.github.com>
2025-03-04 11:10:18 -0500

    .

--- Udemy/Curso_Programacion/sites/js/index.js
+++ Udemy/Curso_Programacion/sites/js/index.js
@@ -1,4 +1,6 @@
 let valor_1 = 1
 let Valor_2 = 5
 let Resultado = valor_1 + Valor_2
-console.log(Resultado)
+document.write('<h1>Introduccion a JavaScript</h1>')
+document.write('<h4>El resultado de la suma es: ' + Resultado + '</h4>')
+// console.log(Resultado)
