5a58d3571f5dae358724bc12674366d6cea79940
Andres2310ra <77172059+Andres2310ra@users.noreply.github.com>
2025-03-04 12:36:44 -0500

    .

--- Udemy/Curso_Programacion/sites/js/index.js
+++ Udemy/Curso_Programacion/sites/js/index.js
@@ -1,6 +1,25 @@
-let valor_1 = 1
-let Valor_2 = 5
-let Resultado = valor_1 + Valor_2
-document.write('<h1>Introduccion a JavaScript</h1>')
-document.write('<h4>El resultado de la suma es: ' + Resultado + '</h4>')
-// console.log(Resultado)
+'Use Strict'
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let valor_1 = Math.floor(Math.random() * 100) + 1;
+    let Valor_2 = Math.floor(Math.random() * 100) + 1;
+    let Resultado = valor_1 + Valor_2;
+
+    document.getElementById('resultado').textContent = 'El resultado de la suma es: ' + Resultado;
+    document.getElementById('valor_1').textContent = 'El valor 1 aleatorio es: ' + valor_1;
+    document.getElementById('valor_2').textContent = 'El valor 2 aleatorio es: ' + Valor_2;
+
+    document.getElementById('actualizar_page').addEventListener('click', function () {
+
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Cargando Nuevo Resultado",
+            showConfirmButton: false,
+            timer: 1500
+        }).then(function () {
+            location.reload();
+        });
+    });
+});
