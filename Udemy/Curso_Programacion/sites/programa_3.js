a813d5d71f825e5f13088c1b6cebe7af29a719d4
Andres2310ra <77172059+Andres2310ra@users.noreply.github.com>
2025-03-13 17:28:36 -0500

    .

--- Udemy/Curso_Programacion/sites/programa_3.js
+++ Udemy/Curso_Programacion/sites/programa_3.js
@@ -1,15 +1,14 @@
-"use strict";
+'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-
-    let numero_1 = document.getElementById('number_1');
-    let numero_2 = document.getElementById('number_2');
+    let valor_1 = document.getElementById('number_1');
+    let valor_2 = document.getElementById('number_2');
     let form_calculo = document.getElementById('form_calculo');
     let boton_inicio = document.getElementById('boton_inicio');
     let boton_reinicio = document.getElementById('boton_volver');
+    let contenedor = '';
+    let comparador_1 = 0;
+    let comparador_2 = 0;
 
-    numero_1.value = Math.floor(Math.random() * 1000);
-    numero_2.value = Math.floor(Math.random() * 1000);
-    
     form_calculo.style.display = 'none';
 
     boton_inicio.addEventListener('click', function () {
@@ -18,40 +17,60 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     form_calculo.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+        event.preventDefault();
 
-        let valor_1 = parseFloat(numero_1.value);
-        let valor_2 = parseFloat(numero_2.value);
 
-        if (valor_1 > valor_2) {
-            Swal.fire({
-                title: "El numero mayor es: " + valor_1 + " y el numero menor es: " + valor_2,
-                icon: "success",
-                draggable: true
-            });
-        } else if (valor_1 < valor_2) {
-            Swal.fire({
-                title: "El numero mayor es: " + valor_2 + " y el numero menor es: " + valor_1,
-                icon: "success",
-                draggable: true
-            });
-        } else if (valor_1 === valor_2) {
-            Swal.fire({
-                title: "Los numeros son iguales",
-                icon: "error",
-                draggable: true
-            });
-        } else {
-            Swal.fire({
-                title: "No puede haber campos vacios",
-                icon: "error",
-                draggable: true
-            });
+        if (Number(valor_1.value) === 0 || Number(valor_2.value) === 0) {
+            valor_1.value = Math.floor(Math.random() * 1000) + 1;
+            valor_2.value = Math.floor(Math.random() * 1000) + 1;
+        } else if (Number(valor_1.value) === comparador_1 && Number(valor_2.value) === comparador_2) {
+            valor_1.value = Math.floor(Math.random() * 1000) + 1;
+            valor_2.value = Math.floor(Math.random() * 1000) + 1;
         }
+
+        let resultado = [];
+
+        if (Number(valor_1.value) <= Number(valor_2.value)) {
+            for (let i = Number(valor_1.value); i <= Number(valor_2.value); i++) {
+                resultado.push(i);
+            }
+        } else {
+            for (let i = Number(valor_2.value); i <= Number(valor_1.value); i++) {
+                resultado.push(i);
+            }
+        }
+
+        comparador_1 = Number(valor_1.value);
+        comparador_2 = Number(valor_2.value);
+
+        console.log(resultado);
+
+        contenedor = resultado.join(', ');
+
+        let mensaje = '';
+
+        if (Number(valor_1.value) < Number(valor_2.value)) {
+                mensaje= "De " + Number(valor_1.value) + " a " + Number(valor_2.value) + " hay " + resultado.length + " numeros."
+        } else {
+                mensaje= "De " + Number(valor_2.value) + " a " + Number(valor_1.value) + " hay " + resultado.length + " numeros."
+        }
+        
+        Swal.fire({
+            title: mensaje,
+            icon: "success",
+            draggable: true
+        }).then(() => {
+
+            Swal.fire({
+                title: "El rango de numeros es: " + contenedor,
+                icon: "success",
+                draggable: true
+            });
+        });
+
     });
 
     boton_reinicio.addEventListener('click', function () {
         location.reload();
     });
-
 });
