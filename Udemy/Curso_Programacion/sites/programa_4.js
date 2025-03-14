b3638f4c2fc0cef42c4df32d094c5dfaee89f02e
Andres2310ra <77172059+Andres2310ra@users.noreply.github.com>
2025-03-14 15:15:13 -0500

    .

--- Udemy/Curso_Programacion/sites/programa_4.js
+++ Udemy/Curso_Programacion/sites/programa_4.js
@@ -1,15 +1,16 @@
-"use strict";
+'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-
-    let numero_1 = document.getElementById('number_1');
-    let numero_2 = document.getElementById('number_2');
+    let valor_1 = document.getElementById('number_1');
+    let valor_2 = document.getElementById('number_2');
     let form_calculo = document.getElementById('form_calculo');
     let boton_inicio = document.getElementById('boton_inicio');
     let boton_reinicio = document.getElementById('boton_volver');
+    let mensaje = '';
+    let mensaje_2 = '';
+    let i = 0;
+    let comparador_1 = 0;
+    let comparador_2 = 0;
 
-    numero_1.value = Math.floor(Math.random() * 1000);
-    numero_2.value = Math.floor(Math.random() * 1000);
-    
     form_calculo.style.display = 'none';
 
     boton_inicio.addEventListener('click', function () {
@@ -18,36 +19,59 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     form_calculo.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+        event.preventDefault();
 
-        let valor_1 = parseFloat(numero_1.value);
-        let valor_2 = parseFloat(numero_2.value);
+        let impares = [];
 
-        if (valor_1 > valor_2) {
-            Swal.fire({
-                title: "El numero mayor es: " + valor_1 + " y el numero menor es: " + valor_2,
-                icon: "success",
-                draggable: true
-            });
-        } else if (valor_1 < valor_2) {
-            Swal.fire({
-                title: "El numero mayor es: " + valor_2 + " y el numero menor es: " + valor_1,
-                icon: "success",
-                draggable: true
-            });
-        } else if (valor_1 === valor_2) {
-            Swal.fire({
-                title: "Los numeros son iguales",
-                icon: "error",
-                draggable: true
-            });
-        } else {
-            Swal.fire({
-                title: "No puede haber campos vacios",
-                icon: "error",
-                draggable: true
-            });
+        console.log(Number(valor_1.value));
+        console.log(Number(valor_2.value));
+
+        if (Number(valor_1.value) === 0 || Number(valor_2.value) === 0) {
+            valor_1.value = Math.floor(Math.random() * 1000) + 1;
+            valor_2.value = Math.floor(Math.random() * 1000) + 1;
+
+        } else if (Number(valor_1.value) === comparador_1 && Number(valor_2.value) === comparador_2) {
+            valor_1.value = Math.floor(Math.random() * 1000) + 1;
+            valor_2.value = Math.floor(Math.random() * 1000) + 1;
         }
+
+        if (Number(valor_1.value) <= Number(valor_2.value)) {
+            for (i = Number(valor_1.value); i <= Number(valor_2.value); i++) {
+                if (i % 2 !== 0) {
+                    impares.push(i);
+                }
+            }
+            mensaje = "Numeros impares entre " + Number(valor_1.value) + " y " + Number(valor_2.value) + " son: " + impares.join(', ');
+
+            mensaje_2 = "De " + Number(valor_1.value) + " a " + Number(valor_2.value) + " hay " + impares.length + " numeros impares.";
+        } else {
+            for (i = Number(valor_2.value); i <= Number(valor_1.value); i++) {
+                if (i % 2 !== 0) {
+                    impares.push(i);
+                }
+            }
+            mensaje = "Numeros impares entre " + Number(valor_2.value) + " y " + Number(valor_1.value) + " son: " + impares.join(', ');
+
+            mensaje_2 = "De " + Number(valor_2.value) + " a " + Number(valor_1.value) + " hay " + impares.length + " numeros impares.";
+        }
+
+        comparador_1 = Number(valor_1.value);
+        comparador_2 = Number(valor_2.value);
+
+        console.log(impares);
+
+        Swal.fire({
+            title: mensaje_2,
+            icon: "success",
+            draggable: true
+        }).then(() => {
+            Swal.fire({
+                title: mensaje,
+                icon: "success",
+                draggable: true
+            });
+        });
+
     });
 
     boton_reinicio.addEventListener('click', function () {
