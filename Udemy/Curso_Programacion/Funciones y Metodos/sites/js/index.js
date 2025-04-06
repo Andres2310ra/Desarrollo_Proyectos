ba5bac57be5b8e2b8a7194be8c55461444daf4da
Andres2310ra <77172059+Andres2310ra@users.noreply.github.com>
2025-04-05 22:16:27 -0500

    .

--- Udemy/Curso_Programacion/Funciones y Metodos/sites/js/index.js
+++ Udemy/Curso_Programacion/Funciones y Metodos/sites/js/index.js
@@ -11,6 +11,21 @@ document.addEventListener('DOMContentLoaded', function () {
     let funcion_flecha = document.getElementById('funcion_flecha');
     let funcion_flecha2 = document.getElementById('funcion_flecha2');
     let btn_funcion_flecha = document.getElementById('btn-funcion_flecha');
+    let a_funcion_indexOf = document.getElementById('a_funcion_indexOf');
+    let a_funcion_lastIndexOf = document.getElementById('a_funcion_lastIndexOf');
+    let a_funcion_find = document.getElementById('a_funcion_find');
+    let a_funcion_finIndex = document.getElementById('a_funcion_finIndex');
+    let a_funcion_includes = document.getElementById('a_funcion_includes');
+    let a_funcion_some = document.getElementById('a_funcion_some');
+    let a_funcion_every = document.getElementById('a_funcion_every');
+    let s_funcion_indexOf = document.getElementById('s_funcion_indexOf');
+    let s_funcion_lastIndexOf = document.getElementById('s_funcion_lastIndexOf');
+    let s_funcion_includes = document.getElementById('s_funcion_includes');
+    let s_funcion_starsWith = document.getElementById('s_funcion_starsWith');
+    let s_funcion_endsWith = document.getElementById('s_funcion_endsWith');
+    let s_funcion_match = document.getElementById('s_funcion_match');
+    let s_funcion_search = document.getElementById('s_funcion_search');
+    let btn_metodos_busqueda = document.getElementById('btn-metodos_busqueda');
     let numero = 0;
     let valor_anterior = 0;
 
@@ -226,8 +241,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }).then(result => {
             if (result.isConfirmed) {
                 let palabra = result.value.split('')
-                funcion_flecha2.innerHTML='';
-                funcion_flecha2.innerHTML=`Se realizo la funcion palabra.forEach(element => {
+                funcion_flecha2.innerHTML = '';
+                funcion_flecha2.innerHTML = `Se realizo la funcion palabra.forEach(element => {
                     console.log(element)
                     funcion_flecha2.innerHTML += element 
                 });, esta imprime como arreglo la palabra ingresada por linea.`
@@ -235,11 +250,80 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log(palabra)
                 palabra.forEach(element => {
                     console.log(element)
-                    funcion_flecha2.innerHTML += '<p>' + element + '</p>' 
+                    funcion_flecha2.innerHTML += '<p>' + element + '</p>'
                 });
 
             }
         })
+    })
+
+    btn_metodos_busqueda.addEventListener('click', function () {
+
+        const frases = [
+            "Las ballenas azules pueden medir hasta 30 metros de longitud.",
+            "Las orcas, aunque se llaman ballenas asesinas, en realidad son delfines grandes.",
+            "Las ballenas jorobadas son famosas por sus impresionantes saltos fuera del agua.",
+            "Las ballenas utilizan sonidos para comunicarse a grandes distancias en el océano.",
+            "Las ballenas minke son una de las especies más pequeñas de ballenas."
+        ];
+
+        let aleatorio = Math.floor(Math.random() * frases.length)
+        let fraseAleatoria = frases[aleatorio]
+
+        console.log(fraseAleatoria)
+
+        function resultSearch(frase) {
+            //Array
+            a_funcion_indexOf.innerHTML = `<br> <b>La busqueda de la frase leida tiene el numero: ${frases.indexOf(fraseAleatoria) + 1}</b>`
+
+            a_funcion_lastIndexOf.innerHTML = `<br> <b>La busqueda de la frase que le sigue a la frase actual es: " ${frases[aleatorio + 1]}", la frase es el numero ${frases.lastIndexOf(frases[aleatorio + 1]) + 1}</b>`
+
+            a_funcion_find.innerHTML = `<br> <b>La busqueda de la frase anterior a la actual es: "${frases.find(frase => frase == frases[aleatorio])}", la frase es el numero ${aleatorio}.</b>`
+
+            a_funcion_finIndex.innerHTML = `<br> <b>El indice de la frase leida es: ${frases.findIndex(frase => frase == fraseAleatoria)}</b>`
+
+            a_funcion_includes.innerHTML=`<b> <br>La frase leida se encuetra en el array: ${frases.includes(fraseAleatoria)}</b>`
+
+            a_funcion_some.innerHTML=`<b> <br> La frase "La ballena de prueba" no se encutra en el array: ${frases.some(frase => frase == "La ballena de prueba")}</b>`
+
+            a_funcion_every.innerHTML=`<b> <br>La palabra "${frase}" se encuentra en todas las frases: ${frases.every(palabra=> palabra.includes(frase))}</br>`
+
+            //String
+
+            s_funcion_indexOf.innerHTML=`<b> <br>El indece de la palabra "${frase}" es el numero: ${fraseAleatoria.indexOf(frase)}</b>`
+
+            s_funcion_lastIndexOf.innerHTML=`<b> <br>El indece de la palabra "${frase}" es el numero: ${fraseAleatoria.lastIndexOf(frase)}</b>`
+
+            s_funcion_includes.innerHTML=`<b> <br>La palabra "${frase}" se encuentra en la frase leida: ${fraseAleatoria.includes(frase)}</b>`
+
+            s_funcion_starsWith.innerHTML=`<b> <br>La frase leida empieza con la palabra "${frase}": ${fraseAleatoria.startsWith(frase)}</b>`
+
+            s_funcion_endsWith.innerHTML=`<b> <br>La frase leida termina con la palabra "${frase}": ${fraseAleatoria.endsWith(frase)}</b>`
+
+            s_funcion_match.innerHTML=`<b> <br>La frase leida tiene la palabra "${frase}": ${fraseAleatoria.match(frase)}</b>`
+
+            s_funcion_search.innerHTML=`<b> <br>La frase leida tiene la palabra "${frase}": ${fraseAleatoria.search(frase)}</b>`
+        }
+
+        Swal.fire({
+            title: "Se mostrara una frase aleatoria sobre ballenas de las 5 frases sobre cargadas en el array",
+            icon: "success",
+            draggable: true
+        }).then(() => {
+            Swal.fire({
+                title: "Lee la siguiente frase y despues ingresa una palabra para buscarla",
+                text: fraseAleatoria,
+                input: 'text',
+                icon: "success",
+                draggable: true
+            }).then(result => {
+                if (result.isConfirmed) {
+                    resultSearch(result.value)
+                }
+            })
+
+        })
+
     })
 
 });
